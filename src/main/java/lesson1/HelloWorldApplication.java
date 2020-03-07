e664d580fd7aeba0f09a56e04c824e4aeaaf02f3
olegmiercoles <oleg.miercoles@dot.com>
2020-03-07 13:32:47 +0100

    Lesson1 materials

--- src/main/java/lesson1/HelloWorldApplication.java
+++ src/main/java/lesson1/HelloWorldApplication.java
@@ -1,18 +1,20 @@
+package lesson1;
+
 import java.util.Scanner;
 
 public class HelloWorldApplication {
     public static void main(String[] args) {
         System.out.println("Hello Java World!");
-	System.out.println("My name is Oleg :)");
+        System.out.println("My name is Oleg :)");
 
-	Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-	String dog = scanner.nextLine();
-	System.out.println(dog);
+        String dog = scanner.nextLine();
+        System.out.println(dog);
 
-	int a = 10;
-	int b = 15;
-	int c = a + b;
-	System.out.println("10 + 15 = " + a + b);
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        System.out.println("10 + 15 = " + a + b);
     }
 }
